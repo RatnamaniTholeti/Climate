44ee0f6214d2a956bf2f3a837113dbbdd9588eeb
THOLETI RATNA MANI <104676857+RatnamaniTholeti@users.noreply.github.com>
2023-07-19 19:58:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function mrng()
+{
+    document.getElementById("wea").src="https://th.bing.com/th/id/OIP.9yLMAw9iaxF7H2bxWzNiCgHaEo?pid=ImgDet&rs=1";
+   
+    
+}
+function an()
+{
+    document.getElementById("wea").src="https://jooinn.com/images/afternoon-sun-6.jpg";
+}
+function night()
+{
+    document.getElementById("wea").src="https://wallpapercave.com/wp/wp2858853.jpg";
+}
